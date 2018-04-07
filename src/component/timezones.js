7676fec5d3b190766659dd7025addf22053bac55
ddcjddb <11126377@qq.com>
2018-04-07 22:23:07 +0800

    signupform and state

---
+++ src/component/timezones.js
@@ -0,0 +1,21 @@
+export default [
+  "(UTC-12:00) International Date Line West",
+
+  "(UTC-11:00) Coordinated Universal Time-11",
+
+  "(UTC-10:00) Hawaii",
+
+  "(UTC-09:00) Alaska",
+
+  "(UTC-08:00) Baja California",
+
+  "(UTC-08:00) Pacific Time (US & Canada)",
+
+  "(UTC-07:00) Arizona",
+
+  "(UTC-07:00) Chihuahua, La Paz, Mazatlan",
+
+  "(UTC-07:00) Mountain Time (US & Canada)",
+
+  "(UTC-06:00) Central America"
+];
